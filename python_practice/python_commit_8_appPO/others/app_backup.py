9a25781932c28ea20ed8ec0ba23456b761f4fd86
qblslion <770260461@qq.com>
2020-08-31 22:56:48 +0800

    commit-8: 企业微信app实战2 PO.py

--- python_practice/python_commit_8_appPO/others/app_backup.py
+++ python_practice/python_commit_8_appPO/others/app_backup.py
@@ -5,23 +5,31 @@ from python_practice.python_commit_8_appPO.page.base_page import BasePage
 from python_practice.python_commit_8_appPO.page.main_page import MainPage
 
 '''
-App类：
-用来存放(特定app): 企业微信app 的通用功能
+App_backup类：改造../page/app.py文件
+配置的数据驱动，抽离公共数据
+把配置的数据放在yaml中，让yaml驱动测试数据
+udid可以算是一个公共变量
 
+########
+数据的数据驱动，把测试数据放到yaml中
+步骤的数据驱动，把page的编写放到yaml中，让yaml驱动page
 '''
-class App(BasePage):
+class App_backup(BasePage):
 
     def start(self):
         if self.driver is None:
-            # caps = {}
+
             caps = dict()
+            # caps["udid"] = "127.0.0.1:7555"  # adb devices查看 udid
+            caps["udid"] = yaml.safe_load(open("./configuration.yaml"))['caps']['udid']
+
+
+
             caps["platformName"] = "android"
             caps["deviceName"] = "emulator-01"
             caps["appPackage"] = "com.tencent.wework"
             caps["appActivity"] = ".launch.LaunchSplashActivity"
             caps["noReset"] = "true"
-            # caps["udid"] = yaml.safe_load(open("../others/configuration.yaml"))['caps']['udid']
-
             self.driver = webdriver.Remote("http://localhost:4723/wd/hub", caps)
             print(self.driver)
             self.driver.implicitly_wait(5)
